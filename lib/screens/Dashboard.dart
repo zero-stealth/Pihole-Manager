b6f340b81dabbf75947a65801086ecac9b865a5e
Lewis <louislaizer4@gmail.com>
2022-06-22 08:39:43 +0300

    :hammer: starter

---
+++ lib/screens/Dashboard.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({ Key? key }) : super(key: key);
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: const [
+          Text("Pi remote")
+        ],
+      ),
+    );
+  }
+}
