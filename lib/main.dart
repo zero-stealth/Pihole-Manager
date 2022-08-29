95ea4808a95b05b9b9c1f598f4bfdd3958d16113
Lewis <louislaizer4@gmail.com>
2022-08-30 01:41:06 +0300

    :hammer: Cache changelog icon to avoid slow load

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    precacheImage(AssetImage('assets/appicon.png'), context);
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
